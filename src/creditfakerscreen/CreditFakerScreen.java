764c77215c05f29a36406a48ba23f63ce24a76ac
Mayur Kadam <mayurkadam200@gmail.com>
2018-06-24 20:23:28 +0530

    Update CreditFakerScreen.java

--- src/creditfakerscreen/CreditFakerScreen.java
+++ src/creditfakerscreen/CreditFakerScreen.java
@@ -1,4 +1,21 @@
+/*
 
+Author : Mighty Ghost Hack 
+Licence : GPL v3 or any later version
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY;
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
 package creditfakerscreen;
 
 import java.io.File;
